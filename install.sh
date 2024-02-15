9b99941542d61746664abc5a02b38df25323b8a0
mack lee <114943402+macklee251@users.noreply.github.com>
2024-02-15 09:25:31 -0300

    feature: Adding install.sh

---
+++ install.sh
@@ -0,0 +1,23 @@
+# Para rodar esse arquivo digitamos . install.sh
+python3 -m venv venv                 # Criando um ambiente virtual
+. venv/bin/activate                  # Ativando o ambiente virtual
+echo "**/__pycache__" > .gitignore   # Criando um arquivo .gitignore
+pip install requirements.txt         # Instalando as dependências do projeto  
+pip install pre-commit               # Instalando o pre-commit
+pre-commit install                   # Instalando o pre-commit
+
+cat <<EOT >> .pre-commit-config.yaml
+repos:
+  - repo: local
+    hooks:
+      - id: requirements
+        name: requirements
+        entry: bash -c 'venv/bin/pip3 freeze > requirements.txt; git add requirements.txt'
+        language: system
+        pass_filenames: false
+        stages: [commit]
+EOT
+
+git init
+git add .
+git commit -m "Config: initial configurations"
