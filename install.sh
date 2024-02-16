8d7df87ddd27de32e61e1884676947c96ccd3b8a
mack lee <114943402+macklee251@users.noreply.github.com>
2024-02-16 06:18:35 -0300

    config: Adding and configuring dockerignore

--- install.sh
+++ install.sh
@@ -1,11 +1,14 @@
 # Para rodar esse arquivo digitamos . install.sh
 echo "**/__pycache__" > .gitignore;   # Criando um arquivo .gitignore
-python3 -m venv venv                 # Criando um ambiente virtual
-. venv/bin/activate                  # Ativando o ambiente virtual
-pip install -r requirements.txt         # Instalando as dependências do projeto  
-pip install pre-commit               # Instalando o pre-commit
-pre-commit install                   # Instalando o pre-commit
-
+cat <<EOT >> .dockerignore
+**/__pycache__
+venv
+EOT
+python3 -m venv venv                  # Criando um ambiente virtual
+. venv/bin/activate                   # Ativando o ambiente virtual
+pip install -r requirements.txt       # Instalando as dependências do projeto  
+pip install pre-commit                # Instalando o pre-commit
+pre-commit install                    # Instalando o pre-commit
 cat <<EOT >> .pre-commit-config.yaml
 repos:
   - repo: local
